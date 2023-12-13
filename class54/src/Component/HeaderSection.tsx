faf4274439357a376238b060ae1ea07ee35e2906
Almugan <153624916+Almugan@users.noreply.github.com>
2023-12-13 22:35:00 +0500

    Update HeaderSection.tsx

--- class54/src/Component/HeaderSection.tsx
+++ class54/src/Component/HeaderSection.tsx
@@ -6,13 +6,18 @@ const HeaderSection = () => {
     return (
         <header className='pt-[60px]'>
             <Container className='flex justify-between items-center'>
-            <div>Hamza</div>
+                <div>
+            <div className='text-primary text-[36px] font-[600]'>Looking for Experienced  Developer</div>
+            <div className='text-primary font-[500px] text-[30px] '>My name is </div>
+            <div className='text-primary text-[72px] font-[600px] '>Hamza Manzoor</div>
+            </div>
             <div className='max-w-[560px] w-full relative'>
             <HeaderBG/>
             <div className='absolute left-[100px] top-[70px] border-4 border-white w-[374px] h-[453px] bg-[#0C8CE9B2] rounded-[21px] overflow-hidden'>
 <img src={UserImg} className='w-full absolute right-[-14px] bottom-[-6px]' alt="UserImg" />
             </div>
             </div>
+
             </Container>
         </header>
     );
